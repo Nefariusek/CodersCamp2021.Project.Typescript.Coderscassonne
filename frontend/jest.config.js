9fa56b8af47547e75daf0fec234df4b2d6b200b0
Nefariusek <szymonpakula98@gmail.com>
2022-03-28 00:14:03 +0200

    Improve initial setup

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,7 +1,7 @@
 // Sync object
 /** @type {import('@jest/types').Config.InitialOptions} */
 
-export const config = {
+module.exports = {
   verbose: true,
   collectCoverage: true,
   coverageThreshold: {
@@ -12,12 +12,17 @@ export const config = {
       statements: 0,
     },
   },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['<rootDir>/src//setupTests.ts'],
+  collectCoverageFrom: ['./src/**'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
     '\\.(scss|sass|css)$': 'identity-obj-proxy',
   },
 };
-
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { FC, ReactElement } from 'react';
-import './App.css';
+
+import { APPLICATION_TITLE } from './constants/labels';
 
 const App: FC = (): ReactElement => {
-  const testVar = 'test;';
+  const testVar = 'test';
 
   return (
     <div className="flex justify-center">
-      <h1 className="font-bold text-2xl text-blue-900">{testVar}</h1>
+      <h1 className="font-bold text-2xl text-blue-900">{APPLICATION_TITLE}</h1>
+      <h1 className="font-bold text-2xl text-blue-900 bg-gray-200 text-red-300">{testVar}</h1>
     </div>
   );
 };

a88fa4a78bd787261a5213939e9e96f455c82df1
GRosza <grzegorz.roszak@coderscrew.pl>
2022-04-11 23:49:45 +0200

    landing page changes

--- frontend/src/views/LandingPage.tsx
+++ frontend/src/views/LandingPage.tsx
@@ -5,7 +5,7 @@ import { APPLICATION_TITLE } from '../constants/labels';
 const LandingPage: React.FunctionComponent = (): ReactElement => (
   <div className="flex justify-center">
     <h1 className="font-bold text-2xl text-blue-900">{APPLICATION_TITLE}</h1>
-    <h1 className="font-bold text-2xl text-blue-900 bg-gray-200 text-red-300">app</h1>
+    <h1 className="font-bold text-2xl text-blue-900 bg-gray-200 text-red-300">APP</h1>
   </div>
 );
 

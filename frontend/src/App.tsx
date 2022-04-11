d4388e1c21d80a8848ee29aeec0468faa528a3af
GRosza <grzegorz.roszak@coderscrew.pl>
2022-04-11 20:39:58 +0200

    add path to constans and changes from review

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,25 @@
 import { FC, ReactElement } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import { PATH_TO_GAMEPAGE, PATH_TO_HOMEPAGE, PATH_TO_LANDINGPAGE } from './constants/paths';
 import GamePage from './views/Game';
 import HomePage from './views/HomePage';
 import LandingPage from './views/LandingPage';
 
 const paths = [
-  { element: <HomePage />, url: '/homepage' },
-  { element: <GamePage />, url: '/gamepage' },
+  { element: <LandingPage />, url: PATH_TO_LANDINGPAGE },
+  { element: <HomePage />, url: PATH_TO_HOMEPAGE },
+  { element: <GamePage />, url: PATH_TO_GAMEPAGE },
 ];
 
 const App: FC = (): ReactElement => (
-  <Router>
+  <BrowserRouter>
     <Routes>
-      <Route path="/" element={<LandingPage />} />
       {paths.map((path) => (
         <Route key={path.url} path={path.url} element={path.element} />
       ))}
     </Routes>
-  </Router>
+  </BrowserRouter>
 );
 
 export default App;

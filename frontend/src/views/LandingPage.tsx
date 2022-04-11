31faae02e7a329413b3d828900f0b516717aca1d
GRosza <grzegorz.roszak@coderscrew.pl>
2022-04-11 23:53:17 +0200

    add TileContainer to landingpage

--- frontend/src/views/LandingPage.tsx
+++ frontend/src/views/LandingPage.tsx
@@ -1,12 +1,27 @@
-import React, { ReactElement } from 'react';
+import { FC, ReactElement } from 'react';
 
+import DrawPile from '../components/DrawPile/DrawPile';
+import TileContainer from '../components/TileContainer';
 import { APPLICATION_TITLE } from '../constants/labels';
+import Locations from '../constants/locations';
+import Tile from '../model/Tile';
 
-const LandingPage: React.FunctionComponent = (): ReactElement => (
-  <div className="flex justify-center">
-    <h1 className="font-bold text-2xl text-blue-900">{APPLICATION_TITLE}</h1>
-    <h1 className="font-bold text-2xl text-blue-900 bg-gray-200 text-red-300">APP</h1>
-  </div>
-);
+const LandingPage: FC = (): ReactElement => {
+  const testVar = 'test';
+
+  const tile = new Tile(
+    { bottom: Locations.CITY, left: Locations.CITY, right: Locations.CITY, top: Locations.CITY },
+    Locations.CITY,
+    true,
+  );
+  return (
+    <div className="flex justify-center">
+      <h1 className="font-bold text-2xl text-blue-900">{APPLICATION_TITLE}</h1>
+      <h1 className="font-bold text-2xl text-blue-900 bg-gray-200 text-red-300">{testVar}</h1>
+      <TileContainer tile={tile} />
+      <DrawPile numberOfAvailableTiles={10} />
+    </div>
+  );
+};
 
 export default LandingPage;

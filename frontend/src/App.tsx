7a80a2e110e9350258e3e64896e657617a87f5b9
GRosza <grzegorz.roszak@coderscrew.pl>
2022-04-11 09:34:37 +0200

    add router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,18 @@
 import { FC, ReactElement } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import { APPLICATION_TITLE } from './constants/labels';
+import Game from './views/Game';
+import HomePage from './views/HomePage';
+import LandingPage from './views/LandingPage';
 
-const App: FC = (): ReactElement => {
-  const testVar = 'test';
-
-  return (
-    <div className="flex justify-center">
-      <h1 className="font-bold text-2xl text-blue-900">{APPLICATION_TITLE}</h1>
-      <h1 className="font-bold text-2xl text-blue-900 bg-gray-200 text-red-300">{testVar}</h1>
-    </div>
-  );
-};
+const App: FC = (): ReactElement => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/homepage" element={<HomePage />} />
+      <Route path="/game" element={<Game />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
---
+++ frontend/src/views/Game.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react';
+
+const Game: React.FunctionComponent = (): ReactElement => (
+  <div>
+    <p>GAMEPAGE</p>
+  </div>
+);
+
+export default Game;

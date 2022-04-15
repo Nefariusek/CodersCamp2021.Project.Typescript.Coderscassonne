249dbb797e1a389b1b6a0e3e6f8a89fce56b391e
Sylwia <mangoweb@wp.pl>
2022-04-15 14:09:18 +0200

    add types to DataStore Context

--- frontend/src/components/DataStoreContext/DataStoreContext.tsx
+++ frontend/src/components/DataStoreContext/DataStoreContext.tsx
@@ -1,12 +1,37 @@
 import { createContext, useMemo, useState } from 'react';
 
+import type Technologies from '../../constants/technologies';
+import type Meeple from '../../model/Meeple';
+import { BoardSize, TileAmount, TurnLength } from '../../model/Settings';
+import type Tile from '../../model/Tile';
+
+export interface PlayersDataInterface {
+  name: string;
+  technology: Technologies;
+  score: number;
+  placedTiles: Tile[];
+  meeples: Meeple[];
+}
+export interface GameSettingsInterface {
+  turnLength: TurnLength;
+  tileAmount: TileAmount;
+  boardSizeX: BoardSize;
+  boardSizeY: BoardSize;
+}
+interface DataStoreContextInterface {
+  playersData: PlayersDataInterface | null;
+  setPlayersData: React.Dispatch<React.SetStateAction<PlayersDataInterface | null>>;
+  gameSettings: GameSettingsInterface | null;
+  setGameSettings: React.Dispatch<React.SetStateAction<GameSettingsInterface | null>>;
+}
+
 type DataStoreProviderProps = { children: React.ReactNode };
 
-const DataStoreContext = createContext(null);
+const DataStoreContext = createContext<DataStoreContextInterface | null>(null);
 
 export const DataStoreProvider = ({ children }: DataStoreProviderProps) => {
-  const [playersData, setPlayersData] = useState(null);
-  const [gameSettings, setGameSettings] = useState(null);
+  const [playersData, setPlayersData] = useState<PlayersDataInterface | null>(null);
+  const [gameSettings, setGameSettings] = useState<GameSettingsInterface | null>(null);
 
   const storeDataWithMemo = useMemo(
     () => ({

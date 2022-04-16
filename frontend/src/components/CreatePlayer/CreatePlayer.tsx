8e3659376b1e54efb2430807dd6a566b1db79044
MariaBanaszkiewicz <xmox@interia.pl>
2022-04-16 20:16:33 +0200

    add adding players

--- frontend/src/components/CreatePlayer/CreatePlayer.tsx
+++ frontend/src/components/CreatePlayer/CreatePlayer.tsx
@@ -1,31 +1,66 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import { useState } from 'react';
 
 import Technologies from '../../constants/technologies';
 
 interface CreatePlayerProps {
   availableTechnologies: Technologies[];
+  addPlayer: (arg0: string, arg1: Technologies) => void;
 }
 
-const CreatePlayer = ({ availableTechnologies }: CreatePlayerProps) => {
-  const [playersName, setPlayersName] = useState('');
+const CreatePlayer = ({ availableTechnologies, addPlayer }: CreatePlayerProps) => {
+  const [playerName, setPlayerName] = useState('');
+  const [playerMeeple, setPlayerMeeple] = useState<Technologies>();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!playerName) {
+      alert('Choose player name!');
+      return;
+    }
+    if (!playerMeeple) {
+      alert('Choose meeple!');
+      return;
+    }
+    console.log(playerName, playerMeeple);
+    addPlayer(playerName, playerMeeple);
+    setPlayerName('');
+    const radio: HTMLInputElement | null = document.querySelector('input[type=radio]:checked');
+    if (radio) {
+      radio.checked = false;
+    }
+    setPlayerMeeple(undefined);
+  };
+
   return (
-    <div className="flex flex-col items-center justify-around w-96 border-4 border-DARKTHEME_LIGHT_GREEN_COLOR">
+    <form
+      className="flex flex-col items-center justify-around w-96 border-4 border-DARKTHEME_LIGHT_GREEN_COLOR"
+      onSubmit={handleSubmit}
+    >
       <p className="font-ALMENDRA font-bold text-4xl text-DARKTHEME_LIGHT_GREEN_COLOR p-3 mb-10">Create player</p>
       <div>
         <input
           className="font-ALMENDRA font-bold text-2xl bg-DARKTHEME_DARK_GREEN_COLOR w-80 text-DARKTHEME_LIGHT_GREEN_COLOR focus:outline-none h-10"
-          value={playersName}
+          value={playerName}
           onChange={(e: React.FormEvent<HTMLInputElement>) => {
-            setPlayersName(e.currentTarget.value);
+            setPlayerName(e.currentTarget.value);
           }}
         />
         <p className="font-ALMENDRA font-bold text-DARKTHEME_LIGHT_GREEN_COLOR p-3">Player Name</p>
       </div>
       <div>
-        <div className="flex gap-1">
-          {availableTechnologies.map((technology) => (
+        <div className="flex justify-around w-80">
+          {availableTechnologies.map((technology: Technologies) => (
             <label>
-              <input className="w-0 h-0 opacity-0  peer" type="radio" name="choose-meeple" value={technology} />
+              <input
+                className="w-0 h-0 opacity-0  peer"
+                type="radio"
+                name="choose-meeple"
+                value={technology}
+                onChange={(e: React.FormEvent<HTMLInputElement>) => {
+                  setPlayerMeeple(e.currentTarget.value as Technologies);
+                }}
+              />
               <img
                 className="peer-checked:border-2 peer-checked:border-DARKTHEME_LIGHT_GREEN_COLOR"
                 src={`./Elements/Meeple/${technology}_meeple.png`}
@@ -42,7 +77,7 @@ const CreatePlayer = ({ availableTechnologies }: CreatePlayerProps) => {
       >
         Add Player
       </button>
-    </div>
+    </form>
   );
 };
 
--- frontend/src/views/CreatePlayersPage.tsx
+++ frontend/src/views/CreatePlayersPage.tsx
@@ -2,20 +2,27 @@ import { useState } from 'react';
 
 import CreatePlayer from '../components/CreatePlayer/CreatePlayer';
 import Technologies from '../constants/technologies';
-// import Player from '../model/Player';
+import Player from '../model/Player';
 
 const CreatePlayersPage = () => {
-  // const [players, setPlayers] = useState<Player[]>([]);
-  const [availableTechnologies] = useState<Technologies[]>([
+  const [players, setPlayers] = useState<Player[]>([]);
+  const [availableTechnologies, setAvailableTechnologies] = useState<Technologies[]>([
     Technologies.HTML,
     Technologies.JS,
     Technologies.TS,
     Technologies.NODE,
     Technologies.REDUX,
   ]);
+
+  const addPlayer = (playerName: string, technology: Technologies) => {
+    const player: Player = new Player(playerName, technology);
+    setPlayers([...players, player]);
+    setAvailableTechnologies(availableTechnologies.filter((tech) => tech !== technology));
+  };
+
   return (
     <div className="bg-black flex items-center justify-center">
-      <CreatePlayer availableTechnologies={availableTechnologies} />
+      <CreatePlayer availableTechnologies={availableTechnologies} addPlayer={addPlayer} />
     </div>
   );
 };

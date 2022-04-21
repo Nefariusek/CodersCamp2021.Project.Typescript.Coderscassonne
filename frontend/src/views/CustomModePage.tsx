f363054c61b3739143c32dce15ba6c77dbc86dab
NataliaCichonska <natalia.cichonska1103@gmail.com>
2022-04-21 10:53:41 +0200

    edd reuseble component for from

--- frontend/src/views/CustomModePage.tsx
+++ frontend/src/views/CustomModePage.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactElement, useState } from 'react';
+import { Dispatch, FC, ReactElement, SetStateAction, useState } from 'react';
 
 import GameMode from '../model/GameMode';
 import { BoardSize, TileAmount, TurnLength } from '../model/Settings';
@@ -7,20 +7,49 @@ const sizes: BoardSize[] = [4, 8, 16, 32, 64];
 const amounts: TileAmount[] = [16, 32, 64, 128];
 const lengths: TurnLength[] = [5, 10, 15, 30, 60];
 
+interface CustomModeFormProps {
+  values: BoardSize[] | TileAmount[] | TurnLength[];
+  property: 'Board size' | 'Tile amount' | 'Turn Length';
+  setFunction:
+    | Dispatch<SetStateAction<BoardSize>>
+    | Dispatch<SetStateAction<TileAmount>>
+    | Dispatch<SetStateAction<TurnLength>>;
+}
+
+const CustomModeForm = (props: CustomModeFormProps) => {
+  const { values, property, setFunction } = props;
+  const [val, setVal] = useState<BoardSize | TileAmount | TurnLength>(32);
+
+  const handleOnClick = (e: any) => {
+    setVal(e.currentTarget.innerHTML);
+    setFunction(e.currentTarget.innerHTML);
+  };
+
+  return (
+    <div className="font-ALMENDRA font-bold text-3xl text-DARKTHEME_LIGHT_GREEN_COLOR p-3 text-center">
+      <p className="text-4xl">{property}</p>
+      <p>{val}</p>
+      <div className="flex flex-col gap-[10px] py-[30px] items-center">
+        {values.map((value) => (
+          <button
+            className="p-3 bg-DARKTHEME_LIGHT_GREEN_COLOR font-ALMENDRA font-bold text-3xl text-DARKTHEME_BACKGROUND_COLOR text-center w-[60px] rounded"
+            type="button"
+            key={value}
+            onClick={handleOnClick}
+          >
+            {value}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
 const CustomModePage: FC = (): ReactElement => {
   const [boardSize, setBoardSize] = useState<BoardSize>(32);
   const [tileAmount, setTileAmount] = useState<TileAmount>(32);
   const [turnLength, setTurnLength] = useState<TurnLength>(30);
 
-  const handleAmountOnClick = (e: any) => {
-    setTileAmount(e.currentTarget.innerHTML);
-  };
-  const handleSizeOnClick = (e: any) => {
-    setBoardSize(e.currentTarget.innerHTML);
-  };
-  const handleLengthOnClick = (e: any) => {
-    setTurnLength(e.currentTarget.innerHTML);
-  };
   const handleGameModeSubmit = () => {
     const gameMode = new GameMode(turnLength, tileAmount, boardSize, boardSize, 'Custom');
     localStorage.setItem('Game mode', JSON.stringify(gameMode));
@@ -32,54 +61,9 @@ const CustomModePage: FC = (): ReactElement => {
         Custom Mode
       </p>
       <div className="flex flex-row justify-around w-full mt-[10px]">
-        <div className="font-ALMENDRA font-bold text-3xl text-DARKTHEME_LIGHT_GREEN_COLOR p-3 text-center">
-          <p className="text-4xl">Board size</p>
-          <p>{boardSize}</p>
-          <div className="flex flex-col gap-[10px] py-[30px] items-center">
-            {sizes.map((size) => (
-              <button
-                className="p-3 bg-DARKTHEME_LIGHT_GREEN_COLOR font-ALMENDRA font-bold text-3xl text-DARKTHEME_BACKGROUND_COLOR text-center w-[60px] rounded"
-                type="button"
-                key={size}
-                onClick={handleSizeOnClick}
-              >
-                {size}
-              </button>
-            ))}
-          </div>
-        </div>
-        <div className="font-ALMENDRA font-bold text-3xl text-DARKTHEME_LIGHT_GREEN_COLOR p-3 text-center">
-          <p className="text-4xl">Tile amount</p>
-          <p>{tileAmount}</p>
-          <div className="flex flex-col gap-[10px] py-[30px] items-center">
-            {amounts.map((amount) => (
-              <button
-                className="p-3 bg-DARKTHEME_LIGHT_GREEN_COLOR font-ALMENDRA font-bold text-3xl text-DARKTHEME_BACKGROUND_COLOR text-center w-[70px] rounded"
-                type="button"
-                key={amount}
-                onClick={handleAmountOnClick}
-              >
-                {amount}
-              </button>
-            ))}
-          </div>
-        </div>
-        <div className="font-ALMENDRA font-bold text-3xl text-DARKTHEME_LIGHT_GREEN_COLOR p-3 text-center">
-          <p className="text-4xl">Turn length</p>
-          <p>{turnLength}</p>
-          <div className="flex flex-col gap-[10px] py-[30px] items-center">
-            {lengths.map((length) => (
-              <button
-                className="p-3 bg-DARKTHEME_LIGHT_GREEN_COLOR font-ALMENDRA font-bold text-3xl text-DARKTHEME_BACKGROUND_COLOR text-center w-[60px] rounded"
-                type="button"
-                key={length}
-                onClick={handleLengthOnClick}
-              >
-                {length}
-              </button>
-            ))}
-          </div>
-        </div>
+        <CustomModeForm values={sizes} property="Board size" setFunction={setBoardSize} />
+        <CustomModeForm values={amounts} property="Tile amount" setFunction={setTileAmount} />
+        <CustomModeForm values={lengths} property="Turn Length" setFunction={setTurnLength} />
       </div>
       <button
         className="p-3 bg-DARKTHEME_LIGHT_GREEN_COLOR font-ALMENDRA font-bold text-4xl text-DARKTHEME_BACKGROUND_COLOR text-center w-[200px] rounded my-[50px]"

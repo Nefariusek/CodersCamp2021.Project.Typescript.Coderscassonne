cdfbc2d2797b96fade90c8c139d3fca14028872f
Sylwia <mangoweb@wp.pl>
2022-05-16 15:02:17 +0200

    set layout proportions

--- frontend/src/constants/gameDefaults.ts
+++ frontend/src/constants/gameDefaults.ts
@@ -2,3 +2,5 @@ import Technologies from './technologies';
 
 export const DEFAULT_TECHNOLOGY = Technologies.HTML;
 export const DEFAULT_PLAYERS_MEEPLE_COUNT = 7;
+export const GAMEBOARD_LAYOUT_PROPORTION = 0.6;
+export const TILE_SIZE = 177;
--- frontend/src/views/Game.tsx
+++ frontend/src/views/Game.tsx
@@ -5,6 +5,7 @@ import GameBoard from '../components/GameBoard/GameBoard';
 import PlayersHand from '../components/PlayersHand/PlayersHand';
 import DrawPile from '../components/DrawPile/DrawPile';
 import Legend from '../components/Legend/Legend';
+import { GAMEBOARD_LAYOUT_PROPORTION, TILE_SIZE } from '../constants/gameDefaults';
 import { MENU_TITLE_SOURCE } from '../constants/layoutElements';
 import { Link } from 'react-router-dom';
 import { PATH_TO_HOMEPAGE } from '../constants/paths';
@@ -19,29 +20,43 @@ const GamePage: React.FunctionComponent = (): ReactElement => {
   const [endOfTurn, setEndOfTurn] = useState<boolean>(false);
   const drawTilesLeft = drawnTiles.length - context.turnNumber > 0 ? drawnTiles.length - context.turnNumber : 0;
   const { turnNumber } = useContext(DataStoreContext);
+
+  const gamebordLayoutProportion = `${GAMEBOARD_LAYOUT_PROPORTION * 100}%`;
+  const playersInfoLayoutProportion = `${((1 - GAMEBOARD_LAYOUT_PROPORTION) / 2) * 100 - 1}%`;
+  const drawPileLayoutProportion = `${((1 - GAMEBOARD_LAYOUT_PROPORTION * 100) / 2) * 100 - 1}%`;
+
   let tilesLeft = drawnTiles.length - turnNumber;
   if (tilesLeft == 1) {
     openEndGameModal();
   }
 
   return (
-    <div>
-      <div className="flex justify-between p-[10px] z-0">
-        <div className="mt-6">
+    <div style={{ height: '97vh' }}>
+      <div
+        className="flex justify-between items-center z-50"
+        style={{ height: playersInfoLayoutProportion, maxHeight: playersInfoLayoutProportion, minHeight: '128px' }}
+      >
+        <div className="flex justify-center">
           <Link to={PATH_TO_HOMEPAGE}>
             <img src={MENU_TITLE_SOURCE} alt="title_tile" className="w-30 h-30" />
           </Link>
         </div>
         <PlayersInfo players={context?.allPlayersData} currentPlayer={currentPlayer} />
         <GameTimer isTurnTimerVisible={false} turnLength={60} setEndOfTurn={setEndOfTurn} />
-        <div className="w-[300px] mt-6 flex justify-end">
+        <div className="flex justify-end">
           <Legend />
         </div>
       </div>
-      <div className="flex justify-center">
+      <div
+        className="flex justify-center items-center"
+        style={{ height: gamebordLayoutProportion, maxHeight: gamebordLayoutProportion, overflow: 'auto' }}
+      >
         <GameBoard endOfTurn={endOfTurn} setEndOfTurn={setEndOfTurn} />
       </div>
-      <div className="flex justify-around p-[10px]">
+      <div
+        className="flex justify-around"
+        style={{ bottom: 0, height: drawPileLayoutProportion, maxHeight: drawPileLayoutProportion }}
+      >
         {!endOfTurn && <PlayersHand />}
         <DrawPile numberOfAvailableTiles={drawTilesLeft} />
       </div>

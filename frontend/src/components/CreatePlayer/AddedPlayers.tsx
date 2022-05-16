cbcfeb508565e497b2510bd37ad44da4208a4662
Sylwia <mangoweb@wp.pl>
2022-05-16 17:12:29 +0200

    add background to AddedPlayers component

--- frontend/src/components/CreatePlayer/AddedPlayers.tsx
+++ frontend/src/components/CreatePlayer/AddedPlayers.tsx
@@ -50,7 +50,7 @@ const AddedPlayersItem = ({ player, number, last, change }: AddedPlayersItemProp
 };
 
 const AddedPlayers = ({ players, save, change }: AddedPlayersProps) => (
-  <div className="flex flex-col items-center justify-between w-96 h-[580px] border-4 border-DARKTHEME_LIGHT_GREEN_COLOR">
+  <div className="flex flex-col items-center justify-between w-96 h-[580px] bg-DARKTHEME_BACKGROUND_COLOR border-4 border-DARKTHEME_LIGHT_GREEN_COLOR">
     <p className="font-ALMENDRA font-bold text-4xl text-DARKTHEME_LIGHT_GREEN_COLOR p-3 mb-10">Players</p>
     <div className="flex flex-col items-center justify-around">
       {players.map((player, i) => (

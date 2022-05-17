5eba25f11787cf3ae8248c19c225dd355d9a64e9
Sylwia <mangoweb@wp.pl>
2022-05-17 19:53:00 +0200

    reafactor modal components

--- frontend/src/components/Modal/EndTurnModal.tsx
+++ frontend/src/components/Modal/EndTurnModal.tsx
@@ -1,45 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { openModal, Modal } from './Modal';
 
 export function openEndTurnModal() {
-  const event = new Event('endTurn');
-  window.dispatchEvent(event);
+  openModal('endTurn');
 }
 
-export const EndTurnModal: React.FC = () => {
-  const [isModalOn, setModalOn] = useState(false);
-
-  const handleClose = () => {
-    setModalOn(false);
-  };
-  React.useEffect(() => {
-    window.addEventListener('endTurn', () => {
-      setModalOn(true);
-    });
-  }, []);
-
-  //setTimeout(handleClose, 5000);
-
-  return (
-    <>
-      {isModalOn && (
-        <div className="   bg-DARKTHEME_DARK_GREEN_COLOR opacity-95 fixed inset-0 z-50   ">
-          <div className="flex h-screen opacity-1 justify-center items-center ">
-            <div className="flex-col justify-center  bg-DARKTHEME_BACKGROUND_COLOR  py-12 px-24 border-4 border-DARKTHEME_LIGHT_GREEN_COLOR rounded-xl ">
-              <div className="flex font-ALMENDRA text-2xl  text-DARKTHEME_LIGHT_GREEN_COLOR  mb-10">
-                It's the end of your turn!
-              </div>
-              <div className="flex justify-center">
-                <button
-                  onClick={handleClose}
-                  className="font-ALMENDRA px-4 py-2 text-xl text-white bg-DARKTHEME_LIGHT_GREEN_COLOR"
-                >
-                  CANCEL
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-    </>
-  );
+export const EndTurnModal = (): React.ReactElement => {
+  return <Modal eventType="endTurn" />;
 };

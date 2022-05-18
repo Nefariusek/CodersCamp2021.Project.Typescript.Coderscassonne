917b80351a5defde84233cc490c4e8552d4f116f
Sylwia <mangoweb@wp.pl>
2022-05-18 16:57:52 +0200

    create nest resources for tiles

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppGateway } from './app.gateway';
+import { TilesModule } from './tiles/tiles.module';
+import { TilesGateway } from './tiles/tiles.gateway';
 
 @Module({
-  imports: [],
+  imports: [TilesModule],
   controllers: [AppController],
-  providers: [AppService, AppGateway],
+  providers: [AppService, AppGateway, TilesGateway],
 })
 export class AppModule {}

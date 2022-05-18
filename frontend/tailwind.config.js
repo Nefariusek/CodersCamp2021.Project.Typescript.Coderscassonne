2fbf4d7a9b8b8a3d4287329db6679e996ee39d73
GRosza <grzegorz.roszak@coderscrew.pl>
2022-05-18 20:27:55 +0200

    Revert "change in tailwind config no longer used purge to content option"
    This reverts commit 5f537b79d525de6c4bde26fb20bad7d97de80acf.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false,
   theme: {
     extend: {
       colors: {

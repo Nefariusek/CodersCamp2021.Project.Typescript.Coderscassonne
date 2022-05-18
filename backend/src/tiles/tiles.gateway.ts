171a1a65381a8f6b06c365af85ff87d2bf842197
Sylwia <mangoweb@wp.pl>
2022-05-18 17:42:44 +0200

    create tile message broadcasting by server

--- backend/src/tiles/tiles.gateway.ts
+++ backend/src/tiles/tiles.gateway.ts
@@ -1,9 +1,11 @@
 import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
 
-@WebSocketGateway()
+import { Socket, Server } from 'socket.io';
+
+@WebSocketGateway(5001, { cors: true })
 export class TilesGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  @SubscribeMessage('sendTilePlaced')
+  handleTilePlacementMessage(client: Socket, text: string): void {
+    client.broadcast.emit('receiveTilePlaced', text);
   }
 }

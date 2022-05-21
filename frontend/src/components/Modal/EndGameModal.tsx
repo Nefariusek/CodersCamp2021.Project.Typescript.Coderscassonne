bff69bb332a71e62ce7254c5b581894ed0afb87d
Sylwia <mangoweb@wp.pl>
2022-05-21 09:44:04 +0200

    make ShowScore modal and EndGame modal be able to have additional content

--- frontend/src/components/Modal/EndGameModal.tsx
+++ frontend/src/components/Modal/EndGameModal.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { observer } from 'mobx-react';
 import { openModal, Modal, ModalEvents } from './Modal';
 
 export function openEndGameModal() {
   openModal(ModalEvents.END_GAME);
 }
 
-export const EndGameModal = (): React.ReactElement => {
-  return <Modal eventType={ModalEvents.END_GAME} />;
-};
+export const EndGameModal = observer((): React.ReactElement => {
+  return (
+    <Modal eventType={ModalEvents.END_GAME}>
+      <div className="flex flex-col"></div>
+    </Modal>
+  );
+});

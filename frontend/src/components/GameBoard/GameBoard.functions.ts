737a697ce01e6cfa65ff2887606750b8bc58b40b
Urszuja <urszula.jarych@gmail.com>
2022-05-21 20:57:37 +0200

    add evaluation for road and monastery projects

--- frontend/src/components/GameBoard/GameBoard.functions.ts
+++ frontend/src/components/GameBoard/GameBoard.functions.ts
@@ -232,3 +232,77 @@ export const placeMeeple = (tile: Tile, projectId: number) => {
     selectedProject.meeples.push(meeple);
   }
 };
+
+//================= POINTS PHASE =====================//
+
+export const evaluatePoints = (tile: Tile) => {
+  if (rootStore.gameStore.currentPhase !== GamePhases.SCORE_PHASE) {
+    console.log('wrong phase');
+  }
+  evaluateRoadProjects(tile);
+  evaluateCityProjects(tile);
+  evaluateMonasteryProjects();
+  if (rootStore.gameStore.drawPile.length < 1) {
+    evaluateUnfinishedProjects();
+  }
+};
+
+function evaluateRoadProjects(tile: Tile) {
+  const roadProjects = rootStore.projectStore.allProjects.filter(
+    (project) => project.type === Locations.ROAD && project.tiles.includes(tile),
+  );
+
+  if (roadProjects) {
+    roadProjects.forEach((project) => {
+      const roadEnds = project.tiles.filter((t) => t.middle.every((m) => m !== Locations.ROAD));
+
+      if (roadEnds.length > 1) {
+        finishProject(project, 2);
+      }
+    });
+  }
+}
+function evaluateCityProjects(tile: Tile) {
+  const cityProjects = rootStore.projectStore.allProjects.filter(
+    (project) => project.type === Locations.CITY && project.tiles.includes(tile),
+  );
+  if (cityProjects) {
+    cityProjects.forEach((project) => {
+      if (false) {
+        finishProject(project, 2);
+      }
+    });
+  }
+}
+
+function evaluateMonasteryProjects() {
+  const monasteryProjects = rootStore.projectStore.allProjects.filter(
+    (project) => project.type === Locations.MONASTERY,
+  );
+  if (monasteryProjects) {
+    monasteryProjects.forEach((project) => {
+      if (project.meeples.length > 8) {
+        finishProject(project, 1);
+      }
+    });
+  }
+}
+
+function finishProject(project: Project, factor: number) {
+  project.isFinished = true;
+  project.meeples.forEach((meeple) => {
+    meeple.player.updateScore(project.tiles.length * factor);
+    meeple.player.returnMeeple(meeple);
+  });
+}
+export const evaluateUnfinishedProjects = () => {
+  const unfinishedProjects = rootStore.projectStore.allProjects.filter((project) => !project.isFinished);
+  unfinishedProjects.forEach((project) => {
+    if (project.type !== Locations.MONASTERY) {
+      project.meeples.forEach((meeple) => {
+        meeple.player.updateScore(project.tiles.length);
+        meeple.player.returnMeeple(meeple);
+      });
+    }
+  });
+};

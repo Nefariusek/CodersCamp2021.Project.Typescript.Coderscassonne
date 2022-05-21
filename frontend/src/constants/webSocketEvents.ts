474f3c1a336652267c832912313e3abbd51e213c
Sylwia <mangoweb@wp.pl>
2022-05-21 17:18:07 +0200

    add room events to WebSocketEvent type

--- frontend/src/constants/webSocketEvents.ts
+++ frontend/src/constants/webSocketEvents.ts
@@ -1,14 +1,24 @@
 enum WebSocketEvent {
   SEND_TILE_PLACED = 'sendTilePlaced',
   RECEIVE_TILE_PLACED = 'receiveTilePlaced',
+
   SEND_TILE_ROTATED = 'sendTileRotated',
   RECEIVE_TILE_ROTATED = 'receiveTileRotated',
-  SEND_MEEPLE_PLACED = 'sendMeeplePlaced',
-  RECEIVE_MEEPLE_PLACED = 'receiveMeeplePlaced',
+
+  SEND_MEEPLE_PLACED = 'sendMeeplePlaced', // 'meeplePlacementMessage',
+  RECEIVE_MEEPLE_PLACED = 'receiveMeeplePlaced', // 'messageToClientAfterMeeplePlacement',
+
   SEND_NEXT_PHASE = 'sendNextPhase',
   RECEIVE_NEXT_PHASE = 'receiveNextPhase',
+
+  SEND_MESSAGE_TO_ROOM = 'sendMessageToRoom', // 'messageToRoom',
+  RECEIVE_MESSAGE_TO_ROOM = 'receiveMessageToRoom', // 'messageToRoom',
+
   SEND_MESSAGE = 'sendMessage',
   RECEIVE_MESSAGE = 'receiveMessage',
+
+  CREATE_ROOM = 'creteRoom',
+  JOINED_ROOM = 'joinedRoom',
 }
 
 export default WebSocketEvent;

ea712d1f4ca2a5b7a02eb9e0460c96c421b7a0f5
Sylwia <mangoweb@wp.pl>
2022-05-23 13:48:37 +0200

    create startGameWithTilesRetrieval function

--- frontend/src/api/getTiles.ts
+++ frontend/src/api/getTiles.ts
@@ -1,19 +1,12 @@
-// import { useEffect, useState } from 'react';
 import { DEFAULT_HEADERS } from '../constants/restResources';
 import Tile from '../model/Tile';
 
-const defaultURL = `https://jsonplaceholder.typicode.com/users?_limit=5`;
 const emptyTiles: Tile[] = [];
 
-async function getTiles(url = defaultURL) {
-  // const [data, setData] = useState(emptyTiles);
-  // const [error, setError] = useState(null);
+async function getTiles(url: string) {
   let data = emptyTiles;
   let error = null;
 
-  console.log(url);
-
-  // useEffect(() => {
   await fetch(url, {
     method: 'GET',
     headers: DEFAULT_HEADERS,
@@ -27,19 +20,11 @@ async function getTiles(url = defaultURL) {
     .then((jsonData) => {
       data = jsonData;
       error = null;
-      // setData(jsonData);
-      // setError(null);
     })
     .catch((err) => {
       data = emptyTiles;
       error = err.message;
-      // setError(err.message);
-      // setData(emptyTiles);
     });
-
-  // }, [url]);
-
-  console.log('DATA Z WNĘTRZA GETA: ', data);
 
   return { data, error };
 }

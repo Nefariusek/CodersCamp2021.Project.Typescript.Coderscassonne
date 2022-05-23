cca829f9a21c950353686940707f56865f324c05
Sylwia <mangoweb@wp.pl>
2022-05-23 13:05:21 +0200

    create api for tiles fom file in Nest

--- backend/src/tiles/tiles.service.ts
+++ backend/src/tiles/tiles.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { tilesData } from '../constants/tilesData';
 
 @Injectable()
-export class TilesService {}
+export class TilesService {
+  getTiles() {
+    return tilesData;
+  }
+
+  getSingleTile(id: string) {
+    const tile = tilesData.find((tile) => tile.id === id);
+    return tile;
+  }
+}

c8cdb3af7fa72048ca96510dbbbefa9ecad2d4a0
GRosza <grzegorz.roszak@coderscrew.pl>
2022-05-23 17:59:42 +0200

    remove nested list in dropdown

--- frontend/src/components/DropDown/DropDown.tsx
+++ frontend/src/components/DropDown/DropDown.tsx
@@ -28,37 +28,11 @@ const Dropdown: FunctionComponent<DropdownProps> = observer(() => {
           className="bg-DARKTHEME_LIGHT_GREEN_COLOR font-ALMENDRA z-50 rounded-sm transform scale-0 group-hover:scale-100 absolute 
           transition duration-400 ease-in-out origin-top min-w-32"
         >
-          <li className="rounded-sm relative px-3 py-1 hover:bg-DARKTHEME_DARK_GREEN_COLOR hover:text-white">
-            {availableProjects?.map((project) => (
-              <>
-                <button className="w-full text-left flex items-center outline-none focus:outline-none">
-                  <span className="pr-1 flex-1">{project.type}</span>
-                  <span className="mr-auto">
-                    <svg
-                      className="fill-current h-4 w-4
-                    transition duration-400 ease-in-out"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                    >
-                      <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                    </svg>
-                  </span>
-                </button>
-              </>
-            ))}
-            <ul
-              className="bg-DARKTHEME_LIGHT_GREEN_COLOR text-black z-50 rounded-sm absolute top-0 right-0 
-          transition duration-400 ease-in-out origin-top-left
-          min-w-32
-          "
-            >
-              {availableProjects?.map((meeple) => (
-                <li className="px-3 py-1 text-black hover:bg-DARKTHEME_DARK_GREEN_COLOR hover:text-white">
-                  {meeple.meeples}
-                </li>
-              ))}
-            </ul>
-          </li>
+          {availableProjects?.map((project) => (
+            <li className="rounded-sm relative px-3 py-1 hover:bg-DARKTHEME_DARK_GREEN_COLOR hover:text-white">
+              {project.type}
+            </li>
+          ))}
         </ul>
       </div>
     </div>

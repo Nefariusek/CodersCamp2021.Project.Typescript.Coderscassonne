d224be0a29a1a9a97cd62bb653d7e6bb8c85b520
Sylwia <mangoweb@wp.pl>
2022-05-25 00:41:26 +0200

    create api for tiles fom database in Nest

--- backend/src/tiles-db/tiles-db.controller.ts
+++ backend/src/tiles-db/tiles-db.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
-@Controller('tiles-db')
-export class TilesDbController {}
+import { TilesDbService } from './tiles-db.service';
+
+@Controller('apidb/tiles')
+export class TilesDbController {
+  constructor(private readonly tilesDbService: TilesDbService) {}
+
+  @Get()
+  async getAllTiles() {
+    const tiles = await this.tilesDbService.getTiles();
+    return tiles;
+  }
+
+  @Get(':id')
+  getTile(@Param('id') tileId: string) {
+    return this.tilesDbService.getSingleTile(tileId);
+  }
+}

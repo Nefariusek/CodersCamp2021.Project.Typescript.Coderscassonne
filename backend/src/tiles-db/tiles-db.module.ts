84effab60da402695c495e509c9f9c62e548e259
Sylwia <mangoweb@wp.pl>
2022-05-25 00:34:52 +0200

    create db service

--- backend/src/tiles-db/tiles-db.module.ts
+++ backend/src/tiles-db/tiles-db.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { TilesDbController } from './tiles-db.controller';
 import { TilesDbService } from './tiles-db.service';
+import { TileSchema } from './tile.model';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'Tile', schema: TileSchema }])],
   controllers: [TilesDbController],
   providers: [TilesDbService],
 })
--- backend/src/tiles-db/tiles-db.service.ts
+++ backend/src/tiles-db/tiles-db.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Tile } from './tile.model';
 @Injectable()
-export class TilesDbService {}
+export class TilesDbService {
+  constructor(@InjectModel('Tile') private readonly tileModel: Model<Tile>) {}
+  async getTiles() {
+    const tiles = await this.tileModel.find().exec();
+    console.log(tiles);
+    return tiles.map((tile) => ({
+      id: tile.tileId,
+      edges: tile.edges,
+      middle: tile.middle,
+      isSpecial: tile.isSpecial,
+    }));
+  }
+  async getSingleTile(id: string) {
+    const tile = await this.tileModel.findOne({ tileId: id }).exec();
+    return {
+      id: tile.tileId,
+      edges: tile.edges,
+      middle: tile.middle,
+      isSpecial: tile.isSpecial,
+    };
+  }
+
+  private async findTile(id: string): Promise<Tile> {
+    let tile;
+    try {
+      tile = await this.tileModel.find({ tileId: id }).exec();
+    } catch (error) {
+      throw new NotFoundException('Could not find tile.');
+    }
+    if (!tile) {
+      throw new NotFoundException('Could not find tile.');
+    }
+    return tile;
+  }
+}
